e7ef31bf9e70052f2a98ce380d8f0ea9a5237a2b
jxnsarmiento <jxnsarmiento@gmail.com>
2017-04-17 16:52:42 +0800

    added dialogs

--- build/scripts/scripts.js
+++ build/scripts/scripts.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
 	// tabs scripts -- start
+	$(".alert>span.close").click(function(){
+		$(this).closest('div.alert').fadeOut("fast");
+	});
 	// tabs scripts -- end
 });
